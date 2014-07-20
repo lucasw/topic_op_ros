e42108380ac68fe1cec02ee11a3050f096380a50
lucasw <lucasw_backwards@gmail.com>
2014-07-19 22:36:57 -0700

    interim checkn trying to use AnyMsg but failing

--- README.md
+++ README.md
@@ -4,6 +4,27 @@ topic_op_ros
 Math expressions on ROS topics (and later parameters too?)
 
 
+Test it out:
+  rostopic pub "test1" std_msgs/Float32 "data: 0.5" &
+  rostopic pub "test2" std_msgs/Float32 "data: 1.0" &
+  rosrun topic_op_ros topiop.py "{'a': '/test1', 'b': '/test2'}" '(a + b)
+
+
+  #./topic_op.py "{'a': '/test1', 'b': '/test2'}" '(a + b)'
+
+Misc
+----
+
 Can this be done already?
 
 https://github.com/ros-visualization/rqt_common_plugins/issues/88 is an open issue
+
+Check out rqt_plot parsing of fields names for type independence subscribing:
+https://github.com/ros-visualization/rqt_common_plugins/blob/groovy-devel/rqt_plot/src/rqt_plot/plot.py
+
+Don't need to know the topic type with AnyMsg, but is connection header still there?
+http://docs.ros.org/diamondback/api/rospy/html/rospy.msg.AnyMsg-class.html
+
+
+
+
--- scripts/topic_op.py
+++ scripts/topic_op.py
@@ -6,15 +6,18 @@
 
 import ast
 import sys
+import string
 import rospy
 
 from ast import literal_eval
 
 # TBD have to import all message types with single data fields?
-from std_msgs.msg import Float32
+from std_msgs.msg import *
+#from std_msgs.msg import Float32
 
 class TopicOp:
     def __init__(self, params, expr): # topics):
+        self.subs_pre = {}
         self.subs = {}
         self.data = {}
         self.var = {}
@@ -28,7 +31,8 @@ class TopicOp:
 
             #print i, topic
             self.var[topic] = var
-            self.subs[topic] = rospy.Subscriber(topic, Float32, self.callback)
+            #self.subs[topic] = rospy.Subscriber(topic, Float32, self.callback)
+            self.subs_pre[topic] = rospy.Subscriber(topic, rospy.AnyMsg, self.callbackPre)
             self.data[topic] = None
 
     def doOp(self):
@@ -52,7 +56,28 @@ class TopicOp:
         # http://stackoverflow.com/questions/2371436/evaluating-a-mathematical-expression-in-a-string
         print eval(self.expr, ns)
 
+    def callbackPre(self, msg):
+        msg_type = msg._connection_header['type']
+        msg_type_split =  string.split( msg_type, '/')
+        if msg_type_split[0] != 'std_msgs':
+            print 'unsupported type', msg_type, 'use a non-array std_msgs'
+            return
+        
+        if msg_type_split[1] == 'Header' or \
+                msg_type_split[1] == 'Empty' or \
+                msg_type_split[1] == 'String' or \
+                len(msg_type_split[1]) > 5 and msg_type_split[1][-5:] == 'Array':
+            print 'unsupported type', msg_type
+            return
+
+        topic = msg._connection_header['topic']
+        print 'topic and split', topic, msg_type_split
+        self.subs[topic] = rospy.Subscriber(topic, 
+                eval(msg_type_split[1]), self.callback)
+        self.subs_pre[topic].unregister()
+
     def callback(self, msg):
+        print msg._connection_header['type']
         topic = msg._connection_header['topic']
         #print topic, msg.data
         self.data[topic] = msg.data
