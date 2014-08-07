4a3d8269c53ed765fb3e4d8176a0274356b564dd
lucasw <lucasw_backwards@gmail.com>
2014-08-07 00:36:50 -0700

    http://answers.ros.org/question/91231/rqt-plugin-not-listedfound-in-list-returned-by-rqt-list-plugins/?answer=91386#post-id-91386 has got to to the point of crashing when rosrun topic_op_ros topic_op_ros, can't find TopicOp.ui.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
+setup_args = generate_distutils_setup(
     packages=['topic_op_ros'],
     package_dir={'': 'src'},
 )
 
+setup(**setup_args)
